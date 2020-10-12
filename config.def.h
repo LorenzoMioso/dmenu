65e3018f91a9fb2a237459c1c4da55d18be6aa35
LorenzoMioso <lorenzo.mioso@gmail.com>
2020-10-12 18:55:02 +0200

    automatic commit

--- config.def.h
+++ config.def.h
@@ -1,16 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
-
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "Hack Nerd Font:size=10",
+	"monospace:size=10",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#3A3A3A";
+static const char col_gray3[]       = "#f9f5d7";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#458588";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray4, col_cyan },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
