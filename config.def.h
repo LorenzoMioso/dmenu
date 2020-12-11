9ee31b8994fdd70fa630202fe8f0d086257d89ba
LorenzoMioso <lorenzo.mioso@gmail.com>
2020-12-11 15:19:50 +0100

    automatic commit

--- config.def.h
+++ config.def.h
@@ -1,11 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
-/* -b  option; if 0, dmenu appears at bottom     */
-static int topbar = 0;                      
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
     "Hack Nerd Font:size=10",
-	"monospace:size=10",
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
@@ -20,6 +21,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { col_gray3, col_gray1 },
 	[SchemeSel] = { col_gray4, col_cyan },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSelHighlight] = { "#ffc978", col_cyan },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
